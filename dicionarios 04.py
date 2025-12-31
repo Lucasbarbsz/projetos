fb1f66359ef8c0ad82a1558ec8df5a78d92a6bf1
lucasbarbosz <f.lucascontato0210@gmail.com>
2025-12-31 16:08:41 -0300

    Envio de projetos

---
+++ dicionarios 04.py
@@ -0,0 +1,34 @@
+# 🧠 DESAFIO 6 — Intermediário (importante 🔥)
+
+# Crie um programa que:
+
+# Comece com:
+
+# frutas = {}
+
+
+# Peça ao usuário:
+
+# nome da fruta
+
+# quantidade
+
+# Adicione ao dicionário
+
+# Se a fruta já existir:
+
+# some a quantidade ao valor atual
+
+# 📌 Exemplo:
+
+# Entrada: maçã, 3
+# Entrada: maçã, 2
+# Resultado: {'maçã': 5}
+
+frutas = {}
+
+nome_fruta = input('Me diga o nome da fruta: ')
+quantidade_fruta = input('Me diga a quantidade:')
+
+for fruta in frutas:
+    print(fruta)
